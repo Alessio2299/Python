92a1d3776f88104de132ef0466d007b8a83428d8
Alessio Napoli <alessionapoli99@icloud.com>
2022-04-05 18:19:31 +0200

    First try with Python

---
+++ First_try/esercizio1.py
@@ -0,0 +1,9 @@
+print('Benvenuto al Programma "Moltiplicatore"!')
+print('Restituisce in output il risutltato di una moltiplicazione tra due numeri')
+
+firstValue = input('Inserisci il primo valore --> ')
+secondValue = input('Inserisci il secondo valore --> ')
+total = float(int(firstValue) + int(secondValue))
+
+print('Il risultato della moltiplicazione è: ' + str(total))
+
