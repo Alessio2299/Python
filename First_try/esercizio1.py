b5f0840e21c2dd8db37ddfb0f16b6daa9764fbca
Alessio Napoli <alessionapoli99@icloud.com>
2022-04-05 18:22:54 +0200

    Fixed

--- First_try/esercizio1.py
+++ First_try/esercizio1.py
@@ -3,7 +3,7 @@ print('Restituisce in output il risutltato di una moltiplicazione tra due numeri
 
 firstValue = input('Inserisci il primo valore --> ')
 secondValue = input('Inserisci il secondo valore --> ')
-total = float(int(firstValue) + int(secondValue))
+total = float(int(firstValue) * int(secondValue))
 
 print('Il risultato della moltiplicazione è: ' + str(total))
 
